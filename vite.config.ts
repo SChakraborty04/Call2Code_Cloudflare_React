dc3c3cfbc666f801ea1b5b899fd9e9f75f28ccc7
SChakraborty04 <shaanch04@yahoo.com>
2025-07-06 05:29:51 +0530

    edit2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/Call2Code_Cloudflare_React",
   plugins: [
     react(),
     mode === 'development' &&
